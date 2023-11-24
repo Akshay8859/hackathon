f3bea0e711093fbba54dc3e12abd57c96bf761d2
Akshay <akshayakku7300@gmail.com>
2023-11-24 15:52:08 +0530

    dashboard

---
+++ frontend/src/app/dashboard/page.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link';
+import NavLinks from "@/app/ui/dashboard/nav-links";
+
+
+export default function SideNav() {
+  return (
+    <div className="flex  justify-center h-full flex-col ">
+      <Link
+        className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-30"
+        href="/"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+<path strokeLinecap="round" strokeLinejoin="round" d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5" />
+</svg>
+        <div className="w-32 text-white md:w-40">
+        </div>
+      </Link>
+      <div className="flex grow flex-col justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+        <NavLinks />
+        <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
+        <form>
+          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:py-2 md:px-3">
+            <div className="hidden md:block">Sign Out</div>
+            
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
